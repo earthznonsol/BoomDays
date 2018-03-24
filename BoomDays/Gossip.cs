3bf93d20b490c8b5c35fc1a4853a3b91b50a0e16
Apinunt Odklun <Earth_kung_kub@windowslive.com>
2018-03-24 19:44:53 +0700

    Implement :   Implement 1 (2018-3-24)

--- BoomDays/Gossip.cs
+++ BoomDays/Gossip.cs
@@ -4,9 +4,72 @@ namespace BoomDays
 {
     public class Gossip
     {
-        public int GetBoomSize(int totalPeople)
+        public const int MAX_PERSONS = 10000;
+        public const int MAX_PERSONS_IN_GROUP = 20;
+
+        private int startTotalPeople;
+
+        private int days;
+        private int maxBoomSize;
+
+        private int currBoomSize;
+        private int currBoomSizeDay;
+        
+        private int totalBoomSize;
+
+        public int Days
         {
-            return totalPeople * 20;
+            get
+            {
+                return days;
+            }
+        }
+        public int MaxBoomSize {
+            get
+            {
+                return maxBoomSize;
+            }
+        }
+
+        public Gossip(int startTotalPeople)
+        {
+            this.startTotalPeople = startTotalPeople;
+            days = 0;
+            maxBoomSize = 0;
+            currBoomSize = 0;
+            currBoomSizeDay = 0;
+            totalBoomSize = 0;
+        }
+
+        public void CalculateBoomSize()
+        {
+            CalculateMaxBoomSize(startTotalPeople);
+        }
+
+        private void CalculateMaxBoomSize(int totalPeople)
+        {
+            if (totalBoomSize < MAX_PERSONS)
+            {
+                
+                currBoomSize = GetBoomSize(totalPeople);
+                totalBoomSize += currBoomSize;
+                currBoomSizeDay++;
+                if (currBoomSize > maxBoomSize)
+                {
+                    maxBoomSize = currBoomSize;
+                    days = currBoomSizeDay;
+                }
+                CalculateMaxBoomSize(maxBoomSize);
+            }
+        }
+
+        private int GetBoomSize(int totalPeople)
+        {
+            int bs = totalPeople * MAX_PERSONS_IN_GROUP;
+            if ((totalBoomSize + bs) > MAX_PERSONS)
+                bs = MAX_PERSONS - totalBoomSize;
+
+            return bs;
         }
     }
 }
--- BoomDaysTest/BoomDaysTestMain.cs
+++ BoomDaysTest/BoomDaysTestMain.cs
@@ -10,14 +10,18 @@ namespace BoomDaysTest
         [TestMethod]
         public void TestGetBommSizeAndDay()
         {
-            Gossip gossip = new Gossip();
+            int expectedDays = 3;
+            int expectedBommSize = 8000;
 
-            int expectedBommSize = 400;
-            int people = 20;
+            int start_people = 1;
+            Gossip gossip = new Gossip(start_people);
+            gossip.CalculateBoomSize();
 
-            int actualBoomSize = gossip.GetBoomSize(people);
+            int actualBoomSize = gossip.MaxBoomSize;
+            int actualDay = gossip.Days;
 
             Assert.AreEqual(expectedBommSize, actualBoomSize);
+            Assert.AreEqual(expectedDays, actualDay);
         }
     }
 }
