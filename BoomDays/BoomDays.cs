10441ca4e91f006d52468a3bdfe6d88dfbbd9758
Apinunt Odklun <Earth_kung_kub@windowslive.com>
2018-03-24 16:58:59 +0700

    Initital Commit : BoomDays

---
+++ BoomDays/BoomDays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomDays.Main
+{
+    public class BoomDays
+    {
+        public BoomDays()
+        {
+
+        }
+
+        public int GetBoomSize(int totalPeople)
+        {
+            return totalPeople * 20;
+        }
+    }
+}
